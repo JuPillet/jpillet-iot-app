1226852afd89754e4a591ff8070ff0f8b12cdd8f
JuPillet <50870731+JuPillet@users.noreply.github.com>
2024-10-19 17:39:35 +0200

    Update 7_create_user.sh

--- bonus/scripts/7_create_user.sh
+++ bonus/scripts/7_create_user.sh
@@ -6,9 +6,6 @@ gitlab_api="$gitlab_http/api/v4"
 gitlab_admin="root"
 gitlab_password=$(bash ./scripts/6_get_rootpass.sh FORTOKEN=1)
 
-new_user=$0
-new_pass=$1
-
 echo 'grant_type=password&username='$gitlab_admin'&password='$gitlab_password > ../../auth.txt
 
 oauth_token=$(curl -s --request POST "$gitlab_host/oauth/token" --data "@../../auth.txt")
@@ -19,8 +16,8 @@ echo "access token : $access_token"
 
 curl --request POST "$gitlab_api/users" \
   --form "access_token=$access_token" \
-  --form "username=$2" \
-  --form "password=$3" \
-  --form "name=$1" \
+  --form "username=$1" \
+  --form "password=$2" \
+  --form "name=$3" \
   --form "email=$4" \
   --form "skip_confirmation=true"
